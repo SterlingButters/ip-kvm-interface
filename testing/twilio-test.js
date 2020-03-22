636b2311e647378a4a4ba9780014347233312343
SterlingButters <sterlingbutters@gmail.com>
2020-03-22 12:06:02 -0600

    Update twilio-test.js

--- testing/twilio-test.js
+++ testing/twilio-test.js
@@ -3,9 +3,9 @@ require('dotenv').config({path:__dirname+'/runtime.env'});
 var twilio = require('twilio');
 //https://www.twilio.com/docs/libraries/node
 
-// var accountSid = 'AC596f5a2b27f8cedf1efb9058521cf5ad'; // Your Account SID from www.twilio.com/console
-// var authToken = '43d2ecaf9cce7f9ee4dfe44ba37bfd34';   // Your Auth Token from www.twilio.com/console
+// Your Account SID from www.twilio.com/console
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
+// Your Auth Token from www.twilio.com/console
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 
 var twilio = require('twilio');
